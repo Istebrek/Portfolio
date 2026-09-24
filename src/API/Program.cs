aee8188a16af122d6664d7d92126456ea07ba352
Istebrek <istebrek.akram@outlook.com>
2026-09-25 01:42:07 +0200

    Add DotNetEnv package to read local environment variables

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,8 +1,12 @@
 using API.Helpers;
 using Application;
 using Infrastructure;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load("../../");
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
@@ -11,7 +15,6 @@ builder.Services.AddSwagger();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
 
 var app = builder.Build();
 
@@ -23,8 +26,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
 
 app.MapControllers();
 
